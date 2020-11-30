9d2ed3efda9e677f2af8ee69dd6a7ea6f8868919
Ismail Iqbal <ismail@hsenidmobile.com>
2020-11-30 14:03:59 +0530

    New : added react router to navigate between code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import Homepage from "./pages/homepage/homepage.component";
+import React from "react";
+import {BrowserRouter, Route} from 'react-router-dom'
+
+const HatPage = () =>(
+    <h1>HAT Page</h1>
+)
 function App() {
   return (
     <div >
-     <Homepage/>
+        <BrowserRouter>
+            <Route exact path="/" component={Homepage}/>
+            <Route  path="/hats" component={HatPage}/>
+        </BrowserRouter>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './menu-item.styles.scss'
-const MenuItem =({
-                     title,
-                     imageUrl,
-                     size
-})=>(
-    <div className={`${size} menu-item`}>
+import {withRouter} from 'react-router-dom'
+
+const MenuItem = ({
+                      title,
+                      imageUrl,
+                      size,
+                      history, linkUrl,
+                      match
+                  }) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div style={{
-            backgroundImage:`url(${imageUrl})`
+            backgroundImage: `url(${imageUrl})`
         }} className="background-image">
 
         </div>
@@ -18,4 +22,4 @@ const MenuItem =({
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
