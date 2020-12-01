6501a7e7046574b510023ac36ee647c407c32d5e
Ismail Iqbal <ismail@hsenidmobile.com>
2020-12-01 11:21:52 +0530

    New: added shop page with styles

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import './App.css';
 import Homepage from "./pages/homepage/homepage.component";
 import React from "react";
 import {BrowserRouter, Route} from 'react-router-dom'
+import ShopPage from "./pages/shop/shop.component";
 
-const HatPage = () =>(
-    <h1>HAT Page</h1>
-)
 function App() {
   return (
     <div >
         <BrowserRouter>
             <Route exact path="/" component={Homepage}/>
-            <Route  path="/hats" component={HatPage}/>
+            <Route  path="/shop" component={ShopPage}/>
         </BrowserRouter>
     </div>
   );
