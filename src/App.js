23dbee27cf9f4c6370b87034830e51d8322fd27f
Ismail Iqbal <ismail@hsenidmobile.com>
2020-12-01 23:26:55 +0530

    New: added header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import Homepage from "./pages/homepage/homepage.component";
 import React from "react";
 import {BrowserRouter, Route} from 'react-router-dom'
+
+import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div >
         <BrowserRouter>
+            <Header/>
             <Route exact path="/" component={Homepage}/>
             <Route  path="/shop" component={ShopPage}/>
         </BrowserRouter>
