45a0a15edd2c3014f9f95fb2edaba7e943871ef1
Ismail Iqbal <ismail@hsenidmobile.com>
2020-12-04 12:05:23 +0530

    New: added redux persist to store cart items

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import logger from 'redux-logger';
 import {applyMiddleware, createStore} from "redux";
 import rootReducer from './root-reducer'
+import {persistStore} from "redux-persist";
 
 const middleWares = [logger];
-const store = createStore(rootReducer,applyMiddleware(...middleWares));
 
-export default store;
+export const store = createStore(rootReducer,applyMiddleware(...middleWares));
+
+export const persistor = persistStore(store);
+
+export default {store,persistor};
